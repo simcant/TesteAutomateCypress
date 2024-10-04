2c5483695cfdc38e32dbb4ab3800dcc3609e1de6
simcant <simona.cantoriu@yahoo.com>
2024-10-04 14:11:08 +0300

    Add files via upload

---
+++ test1.cy.js
@@ -0,0 +1,12 @@
+describe("Site Google.com" , ( )=> {
+
+    // Testul numarul 1
+    it("functioneaza cu o cautare basic", () => {
+        cy.visit('https://google.com'); 
+        cy.get('#L2AGLb').click();
+        cy.get('.gLFyf').type('vlog de it').type('{enter}');
+
+        cy.get('.YmvwI').should('exist');
+    })
+
+})
